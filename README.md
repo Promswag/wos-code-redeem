d658f438168e65f7cb99965e0f4466f8a8ecf3fe
Guillaume Baumgartner <promswag@gmail.com>
2025-03-07 20:35:28 +0100

    command redeem working; command add wip

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # wos-code-redeem
+[WIP]
+A Discord bot that redeems Whiteout Survival gift codes for a list of players
+
+The list of players is a csv file attached to THREAD_ID
+
+commands:
+/redeem code
+/add id1 id2 ... idx
--- main.py
+++ main.py
@@ -3,13 +3,11 @@ import requests
 import time
 import pandas
 import asyncio
-import threading
 from io import StringIO
 
 import discord
 from discord.ext import commands
 
-lock = threading.Lock()
 env = {}
 with open(".env", "r") as file:
 	while True:
@@ -26,53 +24,70 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 def generate_md5(form, secret):
     return hashlib.md5((form + secret).encode('utf-8')).hexdigest()
 
-def redeem_request(code, id, responses):
+async def redeem_request(code, id, count_retries=0, max_retries=6):
 	url = "https://wos-giftcode-api.centurygame.com/api/"
 	headers = {"Content-Type": "application/x-www-form-urlencoded"}
 	secret = "tB87#kPtkxqOS2"
-	form = f"cdk={code}&fid={id}&time={str(time.time_ns())}"
+	form = f"cdk={code}&fid={id}&time={str(int(time.time() * 1000))}"
 	form = f"sign={generate_md5(form, secret)}&" + form
 
 	login_response = requests.post(url + "player", headers=headers, data=form)
 	if login_response.status_code != 200:
-		print(login_response)
-		with lock:
-			responses["critical"].append(str(login_response))
-		return
+		if count_retries < max_retries:
+			await asyncio.sleep(10)
+			return await redeem_request(code, id, count_retries + 1)
+		else:
+			print(str(login_response))
+			return -1, "Too many retries, aborting"
 	
 	login_response = login_response.json()
 	if login_response["err_code"] == 40004:
-		with lock:
-			responses["error"].append(f"Could not login player: {id}")
-		return
+		return 1, f"Could not login player : {id}"
 	
 	redeem_response = requests.post(url + "gift_code", headers=headers, data=form)
 	if redeem_response.status_code != 200:
-		with lock:
-			responses["critical"].append(str(redeem_response))
-		return
+		if count_retries < max_retries:
+			await asyncio.sleep(10)
+			return await redeem_request(code, id, count_retries + 1)
+		else:
+			print(str(login_response))
+			print(str(redeem_response))
+			return -1, "Too many retries, aborting"
 	
 	redeem_response = redeem_response.json()
-	user = f"{f'{id}'.rjust(10)} : {login_response['data']['nickname'].replace('\xa0', ' ')}"
+	user = f"{id}; {login_response['data']['nickname'].replace('\xa0', ' ')};"
 
+	# SUCCESS
 	if redeem_response["err_code"] == 20000:
-		with lock:
-			responses["success"].append(user)
+		return 1, user + " Gift code successfully claimed"
+	# USED
+	elif redeem_response["err_code"] == 40005:
+		return -1, f"Gift code {code} has reached max redeem limit"
+	# TIME ERROR
 	elif redeem_response["err_code"] == 40007:
-		with lock:
-			responses["critical"].append("Redeem code is expired")
-	elif redeem_response["err_code"] == 40008 or redeem_response["err_code"] == 40011:
-		with lock:
-			responses["used"].append(user)
+		return -1, f"Gift code {code} is expired"
+	# RECEIVED
+	elif redeem_response["err_code"] == 40008:
+		return 2, user + " Gift code already claimed"
+	# SAME TYPE EXCHANGE
+	elif  redeem_response["err_code"] == 40011:
+		return 2, user + " Gift code already claimed"
+	# CDK NOT FOUND
 	elif redeem_response["err_code"] == 40014:
-		with lock:
-			responses["critical"].append("Redeem code doesn't exist")
+		return -1, f"Gift code {code} doesn't exist"
+	# TIMEOUT RETRY
+	elif redeem_response["err_code"] == 40004:
+		if count_retries < max_retries:
+			await asyncio.sleep(10)
+			return await redeem_request(code, id, count_retries + 1)
+		else:
+			print(str(login_response))
+			print(str(redeem_response))
+			return -1, "Too many retries, aborting"
 	else:
-		with lock:
-			print(login_response)
-			print(redeem_response)
-			responses["error"].append(f"{str(redeem_response["msg"]).ljust(20)} {user}")
-	return
+		print(str(login_response))
+		print(str(redeem_response))
+		return 0, "Unknown Error"
 
 @bot.event
 async def on_ready():
@@ -87,9 +102,9 @@ async def on_message(message):
 				await message.channel.send("Wesh ma poule!")
 	await bot.process_commands(message)
 
-
 @bot.tree.command(name="redeem", description="Redeem a code", extras=[{"name": "code", "required": True}])
 async def redeem(interaction: discord.Interaction, code: str):
+	await interaction.response.send_message(content=":white_check_mark:", ephemeral=True, delete_after=0)
 
 	if code is None:
 		print("Code missing")
@@ -100,74 +115,78 @@ async def redeem(interaction: discord.Interaction, code: str):
 		print("Wrong channel")
 		return
 	
+	embed = discord.Embed(title=":gift: Gift code redeem processing...", color=discord.Color.yellow())
+	embed.add_field(name="", value="")
+	REP = await interaction.channel.send(embed=embed)
+	
 	start = time.time()
 	
-	REP = await channel.send("Starting process...")
-	thread_channel = channel.get_thread(int(env["THREAD_ID"]))
-	message = [message async for message in thread_channel.history(limit=1, oldest_first=True)][0]
+	thread = channel.get_thread(int(env["THREAD_ID"]))
+	message = [message async for message in thread.history(limit=1, oldest_first=True)][0]
 	url = message.attachments[0].url
 	rep = requests.get(url)
 	buffer = StringIO(rep.text)
 	ids = pandas.read_csv(buffer, index_col='ID', skip_blank_lines=True, comment='#').index
 	
 	size = len(ids)
+	counter = {"success" : 0, "used": 0, "error": 0}
 
-	id_per_batch = 30
-	responses = {"success": [], "used": [], "error": [], "critical": []}
-	counter = {"success": 0, "used": 0, "error": 0, "critical": 0}
+	for i, id in enumerate(ids):
+		err, msg = await redeem_request(code, id)
 
-	for i in range(0, size, id_per_batch):
-		batch_ids = ids[i:i + id_per_batch]
-
-		threads = []
-		for id in batch_ids:
-			threads.append(threading.Thread(target=redeem_request, args=[code, id, responses]))
-		for t in threads:
-			t.start()
-		for t in threads:
-			t.join()
-
-		for key, val in responses.items():
-			counter[key] += len(val)
-			if len(val) == 0:
-				responses[key].append('-')
-
-		if counter["critical"] != 0:
+		if err == -1:
+			print(msg)
 			await REP.delete()
-			await channel.send(f"Gift code {code} is expired/not working!")
+			await channel.send(msg)
 			return
-		
-		now = time.time()
-		threshold = now + 60
-		if len(batch_ids) % id_per_batch == 0:
-			while now < threshold:
-				await REP.edit(content=("{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}".format(
-					f":alarm_clock: Process continues in {(threshold - now):.0f} secs... ",
-					f":peach: Progress: {i + len(batch_ids)}/{size}",
-					f":white_check_mark: Success: {counter["success"]}",
-					"```\n" + "\n".join(responses["success"]) + "```",
-					f":sunglasses: Already claimed: {counter["used"]}",
-					"```\n" + "\n".join(responses["used"]) + "```",
-					f":warning: Error: {counter["error"]}",
-					"```\n" + "\n".join(responses["error"]) + "```",
-					f":x: Critical: {counter["critical"]}",
-					"```\n" + "\n".join(responses["critical"])  + "```"
-				)))
-				await asyncio.sleep(3)
-				now = time.time()
+		elif err == 0:
+			counter["error"] += 1
+		elif err == 1:
+			counter["success"] += 1
+		elif err == 2:
+			counter["used"] += 1
 
-		for key, val in responses.items():
-			responses[key].clear()
+		formated_message = "{}\n{}\n{}\n{}\n{}\n".format(
+			f":alarm_clock: Process ongoing for {(time.time() - start):.0f} secs... ",
+			f":peach: Progress: {i + 1}/{size}",
+			f":white_check_mark: Success: {counter["success"]}",
+			f":warning: Already claimed: {counter["used"]}",
+			f":x: Error: {counter["error"]}"
+		)
+		embed.set_field_at(index=0, name="", value=formated_message)
+		await REP.edit(embed=embed)
 				
 	await REP.delete()
-	await channel.send("{}\n{}\n{}\n{}\n{}\n{}\n{}".format(
-		f":gift: Gift code {code} redeemed!",
+	embed.title = f":gift: Gift code {code} redeemed!"
+	embed.color = discord.Color.green()
+	formated_message = "{}\n{}\n{}\n{}\n{}".format(
 		f":hourglass: Process completed in {(time.time() - start):.0f} secs ",
 		f":peach: Progress: {size}/{size}",
 		f":white_check_mark: Success: {counter["success"]}",
-		f":sunglasses: Already claimed: {counter["used"]}",
-		f":warning: Error: {counter["error"]}",
-		f":x: Critical: {counter["critical"]}"
-	))
+		f":warning: Already claimed: {counter["used"]}",
+		f":x: Error: {counter["error"]}"
+	)
+	embed.set_field_at(0, name="", value=formated_message)
+	await channel.send(embed=embed)
+
+@bot.tree.command(name="add", description="Adds a lists of ids (separated by spaces) to the csv file", extras=[{"name": "ids", "required": True}])
+async def add(interaction: discord.Interaction, ids: str):
+	await interaction.response.send_message(content=":white_check_mark:", ephemeral=True, delete_after=0)
+	thread = interaction.channel.get_thread(int(env["THREAD_ID2"]))
+	message = [message async for message in thread.history(limit=1, oldest_first=True)][0]
+	ids = pandas.DataFrame(index=ids.split(' '))
+
+	if message.attachments:
+		url = message.attachments[0].url
+		rep = requests.get(url)
+		buffer_in = StringIO(rep.text)
+		df = pandas.read_csv(buffer_in, index_col='ID', skip_blank_lines=True, comment='#')
+
+		df = pandas.concat([df, ids])
+		with StringIO() as buffer_out:
+			df.to_csv(buffer_out, index=True, index_label='ID')
+			buffer_out.seek(0)
+			await thread.send(file=discord.File(buffer_out, filename="ID.csv"))
+			await message.delete()
 
 bot.run(env["DISCORD_TOKEN"])
